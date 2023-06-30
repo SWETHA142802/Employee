d4a14f6bf1cd58267c67d29aab5a88ff4aeea38d
SWETHA142802 <swethakfeb1428@gmail.com>
2023-06-30 20:23:53 +0530

    swethakfeb1428@gmail.com SWETHA142802

---
+++ src/main/java/com/employee/Employee/Entity/EmployeeEntity.java
@@ -0,0 +1,63 @@
+package com.employee.Employee.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "email_id")
+    private String email_id;
+    @Column(name = "age")
+    private String age;
+
+    public EmployeeEntity(){
+    }
+    public EmployeeEntity(long id, String name, String gender, String email_id, String age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.email_id = email_id;
+        this.age = age;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/employee/Employee/Service/EmployeeServiceImplementation.java
@@ -0,0 +1,42 @@
+package com.employee.Employee.Service;
+
+import com.employee.Employee.Entity.EmployeeEntity;
+import com.employee.Employee.Repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImplementation(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<EmployeeEntity> findAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Optional<EmployeeEntity> findById(Long id) {
+        return employeeRepository.findById(id) ;
+    }
+
+    @Override
+    public EmployeeEntity saveEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    @Override
+    public EmployeeEntity updateEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
